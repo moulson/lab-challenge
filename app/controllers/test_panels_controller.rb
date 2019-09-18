8c5711daea2e9ca7d33a63a09a9328b67df65a50
Daniel Moulson <dan24797@gmail.com>
2019-09-18 22:54:18 +0100

    first working version

--- app/controllers/test_panels_controller.rb
+++ app/controllers/test_panels_controller.rb
@@ -1,17 +1,95 @@
 class TestPanelsController < ApplicationController
 
   def show
-    #Get the hash via ID
+
+    #Get the hash via id
     test_panel = TestPanel::DATA.select{|test| test[:id] == params[:id]}.first
+
+    #make sure it's a valid id
+    if test_panel.nil?
+      render json:{
+        data:{
+          error: "test panel with id '#{params[:id]}' not found"
+        }
+      },
+      status: 400
+      return nil
+    end
 
     #Get an array of tests that the Test panel works with
     tests = []
-    test_panel[:tests].each do |testy|
-      tests.push Test::DATA.select{|x| x[:id] == testy}.first
+    test_panel[:tests].each do |t|
+      tests.push Test::DATA.select{|x| x[:id] == t}.first
     end
 
-    debugger
-    render json: test_panel
-  end
+    #sort tests alphabetically by id
+    tests.sort_by! {|x| x[:id]}
 
+
+    sample_tubes = []
+    sample_volume_requirement = 0
+
+    #Iterate over tests and add values
+    tests.each do |t|
+      sample_tubes.push(t[:sample_tube_type])
+      sample_volume_requirement += t[:sample_volume_requirement]
+    end
+
+    #remove duplicates and sort
+    sample_tubes.uniq!
+    sample_tubes.sort!
+
+    #build relationships hash
+    relationships = []
+    tests.each do |t|
+      hash = {
+        id: t[:id],
+        type: 'test'
+      }
+      relationships.push(hash)
+    end
+
+    data = {
+        type: 'test_panels',
+        id: params[:id],
+        attributes: {
+          price: test_panel[:price],
+          sample_tube_types: sample_tubes,
+          sample_volume_requirement: sample_volume_requirement
+        },
+        relationships: {
+          test:{
+            data:
+              relationships
+          }
+        }
+    }
+
+    #if the 'included' parameter = 'test', append additional info to the response
+    included = []
+    if params[:included] == 'test'
+      tests.each do |t|
+        hash = {
+          type: 'test',
+          id: t[:id],
+          attributes: {
+            name: t[:name],
+            sample_volume_requirement: t[:sample_volume_requirement],
+            sample_tube_type: t[:sample_tube_type]
+          }
+        }
+        included.push(hash)
+      end
+      json = {
+        data: data,
+        included: included
+      }
+    else
+      json = {
+        data: data
+      }
+    end
+    
+    render json: json, status: 200
+  end
 end
--- spec/requests/test_panel_request_spec.rb
+++ spec/requests/test_panel_request_spec.rb
@@ -4,6 +4,17 @@ describe 'Requesting a test panel', type: :request do
   context 'with a valid test panel ID' do
     let(:test_panel_id) { 'TP1' }
     
-    it 'should respond with an HTTP 200 status'
+    it 'should respond with an HTTP 200 status' do
+      get "/test_panels/#{test_panel_id}"
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context 'with an invalid test panel ID' do
+    let(:test_panel_id){ 'FAKE' }
+    it 'should respond with an HTTP 400 status' do
+      get "/test_panels/#{test_panel_id}"
+      expect(response.status).to eq(400) 
+    end
   end
 end
